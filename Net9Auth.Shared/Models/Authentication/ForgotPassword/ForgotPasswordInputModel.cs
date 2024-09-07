0978eaf6c93a26b12e4a2098a1d880b21cff3690
ekapituro <ekapituro@gmail.com>
2024-09-07 21:36:40 +0200

    Update

---
+++ Net9Auth.Shared/Models/Authentication/ForgotPassword/ForgotPasswordInputModel.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Net9Auth.Shared.Models.Authentication.ForgotPassword;
+
+public class ForgotPasswordInputModel
+{
+    [Required] [EmailAddress] public string Email { get; set; } = "";
+}
