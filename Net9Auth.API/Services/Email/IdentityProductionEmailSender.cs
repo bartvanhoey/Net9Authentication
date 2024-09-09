1f379d9fedfa0be1157a3dba67bc63067cba763d
VanhoeyB <bart.vanhoey@graw.de>
2024-09-09 11:03:18 +0200

    Update

--- Net9Auth.API/Services/Email/IdentityProductionEmailSender.cs
+++ Net9Auth.API/Services/Email/IdentityProductionEmailSender.cs
@@ -10,17 +10,22 @@ public class IdentityProductionEmailSender : IEmailSender<ApplicationUser>
 
     public IdentityProductionEmailSender(IConfiguration configuration)
     {
-        var googleEmail = configuration.GetRequiredSection("GoogleSmtp:GoogleEmail").Value ?? throw new ArgumentNullException();
-        var googleAppPassword = configuration.GetRequiredSection("GoogleSmtp:GoogleAppPassword").Value ?? throw new ArgumentNullException();
+        var googleEmail = configuration.GetRequiredSection("GoogleSmtp:GoogleEmail").Value ??
+                          throw new ArgumentNullException($"SMTP email is not set");
+        var googleAppPassword = configuration.GetRequiredSection("GoogleSmtp:GoogleAppPassword").Value ??
+                                throw new ArgumentNullException($"SMTP password is not set");
         _emailSender = new ProductionEmailSender(googleEmail, googleAppPassword);
     }
 
     public Task SendConfirmationLinkAsync(ApplicationUser user, string email, string confirmationLink) =>
-        _emailSender.SendEmailAsync(email, "Confirm your email", $"Please confirm your account by <a href='{confirmationLink}'>clicking here</a>.");
+        _emailSender.SendEmailAsync(email, "Confirm your email",
+            $"Please confirm your account by <a href='{confirmationLink}'>clicking here</a>.");
 
     public Task SendPasswordResetLinkAsync(ApplicationUser user, string email, string resetLink) =>
-        _emailSender.SendEmailAsync(email, "Reset your password", $"Please reset your password by <a href='{resetLink}'>clicking here</a>.");
+        _emailSender.SendEmailAsync(email, "Reset your password",
+            $"Please reset your password by <a href='{resetLink}'>clicking here</a>.");
 
     public Task SendPasswordResetCodeAsync(ApplicationUser user, string email, string resetCode) =>
-        _emailSender.SendEmailAsync(email, "Reset your password", $"Please reset your password using the following code: {resetCode}");
+        _emailSender.SendEmailAsync(email, "Reset your password",
+            $"Please reset your password using the following code: {resetCode}");
 }
