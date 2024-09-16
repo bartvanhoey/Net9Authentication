0e1cc9415ff86eb4e74194a5ef3b2903670b8346
VanhoeyB <bart.vanhoey@graw.de>
2024-09-16 12:58:59 +0200

    Update

---
+++ Net9Auth.API/Migrations/20240916081746_ApiKeyChangeLength.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Net9Auth.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class ApiKeyChangeLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Key",
+                table: "ApiKeys",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(70)",
+                oldMaxLength: 70);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Key",
+                table: "ApiKeys",
+                type: "nvarchar(70)",
+                maxLength: 70,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(150)",
+                oldMaxLength: 150);
+        }
+    }
+}
